76c2489d82b9b2200cd48f94f2818f847efc2983
tcelishchev.nv <xxxla1u2mlxxx@gmail.com>
2022-02-09 23:44:12 +0500

    Решена еще задача

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -170,7 +170,22 @@ fun maxDivisor(n: Int): Int {
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+
+    var x: Int = if (m > n) {
+        m
+    } else {
+        n
+    }
+
+    while (x > 1) {
+        if (m % x == 0 && n % x == 0) {
+            return false
+        }
+        x--
+    }
+    return true
+}
 
 /**
  * Простая
